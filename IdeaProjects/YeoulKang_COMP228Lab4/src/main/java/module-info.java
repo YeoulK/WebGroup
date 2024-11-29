064f8c4466eafb0b896057b9666047ec5b031365
YeoulKang <ykang35@my.centennialcollege.ca>
2024-11-28 23:49:38 -0500

    Remove ideaProjects folder

--- IdeaProjects/YeoulKang_COMP228Lab4/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.yeoulkang_comp228lab4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.yeoulkang_comp228lab4 to javafx.fxml;
-    exports exercise1;
-}
